b61cf1c3d14a5f8873beed09e541f1d9212218c5
shahks-hub <kasaf.shah28@myhunter.cuny.edu>
2023-08-01 17:18:21 -0400

    updated the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <h1 className={styles.title}>InfoGuru</h1>
-        <h2 className={styles.subtitle}>Got documents? Got Questions? Let AI handle the rest</h2>
-        <p className={styles.paragraph}>InfoGuru lets you upload spreadsheets and have it interpret, graph, and explain the data without having to move a finger.</p>
+        <h2 className={styles.subtitle}>Got documents? Got Questions? Let AI handle the rest!</h2>
+        <p className={styles.paragraph}>InfoGuru lets you upload spreadsheets and have it interpret, graph, and explain the data with one click.</p>
         <button className={styles.btn}>Try it now</button>
         <button className ={styles.login}>Login</button>
         <button className ={styles.signup}>SignUp</button>
