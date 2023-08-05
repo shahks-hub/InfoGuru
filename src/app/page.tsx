34ecbd7ffbd43d4a8bb07c3a6cecfa94be8dbe48
Kushal ⠀ <kush.s2805@gmail.com>
2023-08-05 16:49:22 -0500

    Added some code for signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,91 +4,34 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+      <div className={styles.label}>
+        <h1 className={styles.textwrapper}>🧘‍♂️ InfoGuru</h1>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.rectangle}>
+        <div className={styles.signup_title}>
+        <h1>Sign Up</h1>
+        </div>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <label className={styles.input_title}>Name</label>
+        <input className={styles.input} type='name' placeholder='Your Name/Organization Name' />
+      </div>
+      <div>
+      <label className={styles.input_title}>Email</label>
+        <input className={styles.input} type='email' placeholder='Your Email' />
+        </div>
+        <div>
+        <label className={styles.input_title}>Password</label>
+        <input className={styles.input} type='password' placeholder='********' />
+        </div>
+        <div>
+        <label className={styles.input_title}>Confirm Password</label>
+        <input className={styles.input} type='password' placeholder='********' />
+        </div>
+        <div>
+            <button className={styles.signup_button}>Sign Up</button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
