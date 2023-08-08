8998b7767789c63686497bdbe7689ea79a718c69
Kushal ⠀ <kush.s2805@gmail.com>
2023-08-07 20:28:39 -0500

    added signup

---
+++ prisma/migrations/20230808004925_init/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "Account";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "VerificationToken";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,58 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import { FormEvent } from 'react'
 
 export default function Home() {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    const data = new FormData(event.target as HTMLFormElement);
+    event.preventDefault();
+    console.log(data.get("name"))
+
+    const payload = {
+      name: data.get('name'), 
+    email: data.get('email'), 
+    password: data.get('password'), 
+    confirmPass: data.get('confirmPass')}
+
+    const response = await fetch('/api/signup', { method: 'POST', body: JSON.stringify(payload),    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }})
+
+    console.log(response.text)
+
+  }
   return (
     <main className={styles.main}>
       <div>
         <h1 className={styles.title}>🧘‍♂️ InfoGuru </h1>
           <h1 className={styles.signup_wrapper}>Login</h1>
       </div>
-      <div className={styles.container}>
+      <form onSubmit={onSubmit} className={styles.container}>
         <div className={styles.signup_title}>
         <h1>Sign Up</h1>
         </div>
         <div>
           <label className={styles.input_title}>Name</label>
-        <input className={styles.input} type='name' placeholder='Your Name/Organization Name' />
+        <input className={styles.input} name='name' type='name' placeholder='Your Name/Organization Name' />
       </div>
       <div>
       <label className={styles.input_title}>Email</label>
-        <input className={styles.input} type='email' placeholder='Your Email' />
+        <input className={styles.input} name='email' type='email' placeholder='Your Email' />
         </div>
         <div>
         <label className={styles.input_title}>Password</label>
-        <input className={styles.input} type='password' placeholder='********' />
+        <input className={styles.input} name='password' type='password' placeholder='********' />
         </div>
         <div>
         <label className={styles.input_title}>Confirm Password</label>
-        <input className={styles.input} type='password' placeholder='********' />
+        <input className={styles.input} name='confirmPass' type='password' placeholder='********' />
         </div>
         <div>
             <button className={styles.signup_button}>Sign Up</button>
         </div>
-      </div>
+      </form>
     </main>
   )
 }
